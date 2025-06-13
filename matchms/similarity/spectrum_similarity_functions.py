7ed322f24120f92d26c0ebb80040fe7c37374ab8
Julian Pollmann <julian.pollmann@hs-duesseldorf.de>
2025-06-13 22:23:11 +0200

    Test dtypes

--- matchms/similarity/spectrum_similarity_functions.py
+++ matchms/similarity/spectrum_similarity_functions.py
@@ -32,6 +32,8 @@ def collect_peak_pairs(spec1: np.ndarray, spec2: np.ndarray,
     matching_pairs : numpy array
         Array of found matching peaks.
     """
+    print("*" * 100)
+    print(spec1.dtype, spec2.dtype)
     matches = find_matches(spec1[:, 0], spec2[:, 0], tolerance, shift)
     idx1 = [x[0] for x in matches]
     idx2 = [x[1] for x in matches]
@@ -75,6 +77,8 @@ def find_matches(spec1_mz: np.ndarray, spec2_mz: np.ndarray,
         mz = spec1_mz[peak1_idx]
         low_bound = mz - tolerance
         high_bound = mz + tolerance
+        print(low_bound, high_bound)
+        print(type(low_bound), type(high_bound))
         for peak2_idx in range(lowest_idx, spec2_mz.shape[0]):
             mz2 = spec2_mz[peak2_idx] + shift
             if mz2 > high_bound:
